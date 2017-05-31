1949311d1536959ce683541833166fdeb29cb6c5
crystalvjohnston <crystalvjohnston@gmail.com>
2017-06-01 00:28:14 +1200

    Styling not complete

--- server.js
+++ server.js
@@ -15,18 +15,24 @@ app.get('/', function(req, res) {
 }).listen(8080);
 
 app.post('/newPlayer', function(req, res) {
-    fs.readFile('players.json', 'utf8', function readFileCallback(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            var obj = JSON.parse(data);
-            obj.players.push(req.body);
-            json = JSON.stringify(obj);
-            fs.writeFile('players.json', json, 'utf8', (err) => {
-                if (err) throw err;
-                console.log('The file has been saved!');
-            });
-           res.send(data);
-        }
-    });
+    fs.readFile('players.json', 'utf8', readFileCallback);
+
+
+function readFileCallback(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        var obj = JSON.parse(data);
+        obj.players.push(req.body);
+        json = JSON.stringify(obj);
+        fs.writeFile('players.json', json, 'utf8', callback);
+        res.send(data);
+    }
+}
+
+function callback(err) {
+    if (err) throw err;
+    console.log('The file has been saved!');
+};
+
 });
